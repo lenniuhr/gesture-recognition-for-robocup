b3496351c123b274fbbdaa94b1133836c73f82d6
lennartuhrmacher <lennartuhrmacher@gmail.com>
2020-06-03 23:23:57 +0200

    no message

--- fileutil.py
+++ fileutil.py
@@ -18,40 +18,6 @@ class BodyEntry:
 
 #-------- RAW --------#
 
-def update_frame_nr(dataset, file_prefix):
-	db = TinyDB("db/" + dataset + '-raw.json')
-	query = Query()
-	results = db.search(query.filename.matches(file_prefix))
-	i = 1
-	for result in results:
-		print(result["filename"])
-		db.update({'frame_nr': i}, query.filename == result["filename"])
-		i = i + 1
-
-def clean2():
-	dataset = "pose-2"
-	db = TinyDB("db/" + dataset + '-raw.json')
-	entries = db.all()
-	query = Query()
-	print(entries)
-	for entry in entries:
-		db.update(delete('hand_r'), query.filename == entry['filename'])
-		db.update(delete('hand_l'), query.filename == entry['filename'])
-
-	#print(entries)
-
-def clean():
-	dataset = "pose-3"
-	db = TinyDB("db/" + dataset + '-raw.json')
-	query = Query()
-	existing_entries = db.search(query.filename == 531)
-	print(existing_entries)
-	for i in range (480, 540):
-		db.remove(query.filename == i)
-
-	existing_entries = db.search(query.filename == 531)
-	print(existing_entries)
-
 def upsert_raw_entry(dataset, filename, frame_nr, body):
 	db = TinyDB("db/" + dataset + '-raw.json')
 	query = Query()
@@ -91,15 +57,4 @@ def get_all_body_entries(dataset):
 	results = db.all()
 	entries = np.array(list(map(lambda r: BodyEntry(r), results)))
 	return np.array(entries)
-
-#clean2()
-
-#dataset = "pose-3"
-#file_prefixes =  ["cross-arms"]
-#for file_prefix in file_prefixes:
-	#update_frame_nr(dataset, file_prefix)
-	#entries = get_raw_entries(dataset, file_prefix)
-	#for entry in entries:
-		#print(entry.filename)
-		#print(entry.frame_nr)
 
