ec28255ed810d4afc8052166b1a89f31b3f561e7
lennartuhrmacher <lennartuhrmacher@gmail.com>
2020-06-03 18:35:12 +0200

    no message

--- model-3.py
+++ model-3.py
@@ -17,7 +17,7 @@ PATH = "./pytorch-model.pth"
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="True"
 
-body_labels = ["spin", "clap", "time-out", "dance", "idle"]
+body_labels = ["spin", "clap", "time-out", "dance", "idle", "fold"]
 
 def label_to_number(label):
 	return body_labels.index(label)
@@ -30,7 +30,7 @@ output_size = len(body_labels)
 
 k_fold_splits = 4
 
-validation_iter = 10
+validation_iter = 1
 
 sequence_length = 10
 
@@ -38,7 +38,7 @@ batch_size = 10
 
 learning_rate = 0.01
 
-num_layers = 2
+num_layers = 1
 
 class TrainEntry:
 	def __init__(self, label, tensor):
@@ -46,25 +46,22 @@ class TrainEntry:
 		self.tensor = tensor
 
 class RNN(nn.Module):
-	def __init__(self, inputSize, hiddenSize, numLayers):
+	def __init__(self, input_size, hidden_size, num_layers):
 		super().__init__()
-		self.RNN = nn.RNN(input_size=inputSize, 
-						  hidden_size=hiddenSize, 
-						  num_layers=numLayers,
-						  batch_first=True) #inputs and outputs are  (batch, seq, feature)
-		self.r2h = nn.Linear(hiddenSize, hiddenSize)
-		self.h2o = nn.Linear(hiddenSize, output_size)
+		self.RNN = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True) #inputs and outputs are  (batch, seq, feature)
+		self.r2h = nn.Linear(hidden_size, hidden_size)
+		self.h2o = nn.Linear(hidden_size, output_size)
 		self.softmax = nn.LogSoftmax(dim=1)
 		
-	def forward(self, x, hState):
-		x, h = self.RNN(x, hState)
+	def forward(self, x, hidden_state):
+		x, h = self.RNN(x, hidden_state)
 		x = F.relu(self.r2h(x[:,-1,:])) # gets last output
 		x = self.h2o(x)
 		x = self.softmax(x)
 		return x
 
 
-def train_model(num_epochs, num_layers, hidden_size, train_entries, print_loss = True):
+def train_model(num_epochs, num_layers, hidden_size, train_entries, print_loss = False):
 
 	model = RNN(input_size, hidden_size, num_layers)
 	model = model.float()
@@ -81,7 +78,7 @@ def train_model(num_epochs, num_layers, hidden_size, train_entries, print_loss =
 		for train_entry in batched_entries:
 			optimizer.zero_grad()
 			#print(train_entry.tensor.shape)
-			hState = torch.zeros([num_layers, 10, 20])
+			hState = torch.zeros([num_layers, batch_size, hidden_size])
 			outputs = model(train_entry.tensor, hState)
 			#print(train_entry.label)
 			loss = criterion(outputs, train_entry.label)
@@ -91,6 +88,7 @@ def train_model(num_epochs, num_layers, hidden_size, train_entries, print_loss =
 		if print_loss:
 			print("Epoch %i: %f loss" % (epoch, running_loss))
 	return model
+
 
 def get_input():
 
@@ -137,43 +135,6 @@ def batch_entries(batch_size, entries):
 		batched_entries.append(TrainEntry(label, tensor))
 	return np.array(batched_entries)
 
-
-def get_sequences_new():
-
-	train_entries = []
-
-	for label in body_labels:
-		entries = get_body_entries(dataset, label)
-
-		sequences = []
-		for i in range(0, len(entries), sequence_length):
-			frames = []
-			for j in range(i, i + sequence_length):
-				#print(entries[j].label + " - " + str(entries[j].frame_nr))
-				frames.append((entries[j].angles * 2) - 1)
-
-			sequences.append(frames)
-
-		for b in range(0, len(sequences), batch_size):	
-
-			batch_sequences = sequences[b:b + batch_size] 
-
-			tensor = torch.tensor(batch_sequences)
-			tensor = tensor.float()
-
-			labels = []
-			for i in range(0, batch_size):
-				labels.append(label_to_number(label))
-
-			#print(tensor)
-			#print("batch shape: " + str(tensor.shape))
-			#print(labels)
-			#sys.exit(-1)
-
-			train_entries.append(TrainEntry(torch.tensor(labels), tensor))
-
-	return np.array(train_entries)
-
 def get_accuracy(model, test_entries):
 
 	correct = 0
@@ -181,8 +142,8 @@ def get_accuracy(model, test_entries):
 		for test_entry in test_entries:
 
 			label = test_entry.label.item()
-			hState = torch.zeros([num_layers, 1, 20])
-			outputs = model.forward(test_entry.tensor, hState)
+			hidden_state = torch.zeros([num_layers, 1, 20])
+			outputs = model.forward(test_entry.tensor, hidden_state)
 			_, predicted = torch.max(outputs, 1)
 			if (predicted == label):
 				correct += 1
@@ -193,6 +154,8 @@ def get_accuracy(model, test_entries):
 def validate_model(num_epochs, num_layers, hidden_size):
 
 	entries = get_input()
+
+	print("Found %d entries" % len(entries))
 
 	accuracys = []
 	for i in range (validation_iter):
@@ -210,7 +173,7 @@ def validate_model(num_epochs, num_layers, hidden_size):
 
 def hyperopt():
 
-	nums_epochs = [100]
+	nums_epochs = [50]
 	hidden_sizes = [20]#[3, 5, 10, 20]
 
 	results = []
@@ -236,122 +199,4 @@ def hyperopt():
 
 print("---------- START ----------")
 
-#train_entries = get_sequences_new(body_labels)
-
-#print(len(train_entries))
-
-#train_model(50, 2, 20, train_entries)
-
-#validate_model(50, 2, 20)
 hyperopt()
-#print(sequences[0].tensor.shape)
-
-# shape(seq_len, batch, input_size)
-
-#seq_len = 5
-#batch_size = 3
-#input_size = 2
-
-#sequence = [[[1,2],[1,2],[1,2]], [[1,2],[1,2],[1,2]], [[1,2],[1,2],[1,2]], [[1,2],[1,2],[1,2]], [[1,2],[1,2],[1,2]]]
-
-#batch = np.array(sequence)
-
-#print(batch.shape)
-
-#tensor = torch.from_numpy(batch)
-
-#print(tensor.shape)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def get_sequences(labels):
-
-	train_entries = []
-
-	for label in labels:
-		entries = get_body_entries(dataset, label)
-		
-		#entries = entries[0:100]
-		#print("------------- entries ----------------")
-		#print((entries[0].angles * 2) - 1)
-		#print((entries[1].angles * 2) - 1)
-
-		sequences = []
-		for i in range(0, len(entries), sequence_length):
-			frames = []
-			for j in range(i, i + sequence_length):
-				#print(entries[j].label + " - " + str(entries[j].frame_nr))
-				frames.append((entries[j].angles * 2) - 1)
-
-			sequences.append(frames)
-
-
-		#print(np.array(sequences).shape)
-
-		# shape(seq_len, batch_size, input_size)
-		# shape (5, 10, 16)
-
-		#print(len(sequences))
-
-		tensors = []
-		for b in range(0, len(sequences), batch_size):
-			print(b)
-			# all sequences for the batch
-			b_seq = sequences[b:b + batch_size]
-			batch = []
-			# llop through sequence length
-			for f in range(0, sequence_length):
-				batch_entry = []
-				# pick every f-th frame for entry
-				for seq in b_seq:
-					batch_entry.append(seq[f])
-				batch.append(batch_entry)
-
-
-			tensor = torch.tensor(batch)
-			tensor = tensor.float()
-			#print("batch shape: " + str(tensor.shape))
-
-			labels = torch.from_numpy(np.full((1, batch_size), label_to_number(label)))
-			#print("labels shape: " + str(labels.shape))
-
-			train_entries.append(TrainEntry(labels, tensor))
-
-
-
-		#print("------------- train entries ----------------")
-		#print(train_entries[0].tensor)
-
-		#sys.exit(-1)
-
-	return train_entries
